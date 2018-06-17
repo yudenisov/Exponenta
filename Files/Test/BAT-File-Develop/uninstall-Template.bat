a0b50b2c0c06ad2469dccad2165b3e07de370967
Юрий А. Денисов <yudenisov@mail.ru>
2018-06-17 14:35:28 +0400

    Second Commit

---
+++ Files/Test/BAT-File-Develop/uninstall-Template.bat
@@ -0,0 +1,13 @@
+@echo off
+rem Template of command file for Uninstallation packets
+
+rem set main Environment Variable (see installation script)
+
+rem Pre-uninstall actions
+rem Here is uninstalling addition packets, check environment, etc
+
+rem Main uninstall actions
+rem There are doing next step at this section
+rem 1. Copy packet-spicific uninstall script distrib and additional files into the %TEMP% directory
+rem 2. Rename those files to make possible running them
+rem 3. Execute that script and exit from this script
